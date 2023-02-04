18b0b5abd0a059a558fee59baa57eefb3b3cf310
Ion-Alexandru <ion.alexandru2105@gmail.com>
2023-02-04 03:12:34 +0200

    Plants grow update

--- Assets/Script/gardeningBehaviour.cs
+++ Assets/Script/gardeningBehaviour.cs
@@ -11,8 +11,20 @@ public class gardeningBehaviour : MonoBehaviour
 
     public Sprite mudSprite;
     public Sprite grassSprite;
+    public Sprite seedSprite;
+    public Sprite matureSprite;
+
+    public Sprite[] harvestableSprites;
 
     public float speed;
+    public GameObject seedOutline;
+    public GameObject mudOutline;
+    int i = 0;
+    private bool isHoed;
+    private bool isHoeInHand = false;
+    private bool isSeedInHand = false;
+    private bool isScytheInHand = false;
+    float startTime;
 
     private void Start() {
     }
@@ -20,6 +32,14 @@ public class gardeningBehaviour : MonoBehaviour
     private void Update() {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && i < 5){
+            i += 1;
+            Debug.Log("Current seed is:" + i);
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && i > 0){
+            i -= 1;
+            Debug.Log("Current seed is:" + i);
+        }
 
         Vector2 direction = new Vector2(horizontal, vertical);
         if (direction.sqrMagnitude > 0) {
@@ -29,7 +49,44 @@ public class gardeningBehaviour : MonoBehaviour
 
         transform.position = transform.position + new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
 
-        changeTileSprite();
+        if (isHoeInHand){
+            mudOutline.transform.position = tilemap.GetCellCenterWorld(selectedTilePosition);
+
+            changeTileSprite();
+        }
+
+        if (isSeedInHand){
+            seedOutline.transform.position = tilemap.GetCellCenterWorld(selectedTilePosition);
+
+            plantSeed(selectedTilePosition, i);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1) && isHoeInHand == false){
+            isHoeInHand = true;
+            mudOutline.SetActive(true);
+
+        } else if(Input.GetKeyDown(KeyCode.Alpha1) && isHoeInHand) {
+            isHoeInHand = false;
+            mudOutline.SetActive(false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha2) && isSeedInHand == false){
+            isSeedInHand = true;
+            seedOutline.SetActive(true);
+
+        } else if(Input.GetKeyDown(KeyCode.Alpha2) && isSeedInHand) {
+            isSeedInHand = false;
+            seedOutline.SetActive(false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha3) && isScytheInHand == false){
+            isScytheInHand = true;
+            mudOutline.SetActive(true);
+
+        } else if(Input.GetKeyDown(KeyCode.Alpha3) && isScytheInHand) {
+            isScytheInHand = false;
+            mudOutline.SetActive(false);
+        }
     }
 
     private void selectTile() {
@@ -48,6 +105,8 @@ public class gardeningBehaviour : MonoBehaviour
 
             // Change the sprite of the selected tile to the new sprite
             tilemap.SetTile(selectedTilePosition, newTile);
+
+            Debug.Log("Tile position is: " + selectedTilePosition);
         } else if (Input.GetKeyDown(KeyCode.Backspace)){
             // Get the current tile at the selected tile position
             TileBase currentTile = tilemap.GetTile(selectedTilePosition);
@@ -58,6 +117,77 @@ public class gardeningBehaviour : MonoBehaviour
 
             // Change the sprite of the selected tile to the new sprite
             tilemap.SetTile(selectedTilePosition, newTile);
+        }
+    }
+
+    private void plantSeed(Vector3Int tilePosition, int i) {
+        if (Input.GetKeyDown(KeyCode.F)) {
+            // Get the current tile at the selected tile position
+            TileBase currentTile = tilemap.GetTile(tilePosition);
+
+            // Create a new tile that uses the new sprite
+            Tile newTile = ScriptableObject.CreateInstance<Tile>();
+            newTile.sprite = seedSprite;
+
+            // Change the sprite of the selected tile to the new sprite
+            tilemap.SetTile(tilePosition, newTile);
+
+            StartCoroutine(WaitAndChangeTileToMature(10f, tilePosition, i));
+
+        }
+    }
+
+    private IEnumerator WaitAndChangeTileToMature(float waitTime, Vector3Int tilePosition, int i) {
+        yield return new WaitForSeconds(waitTime);
+
+        // Get the current tile at the selected tile position
+        TileBase currentTile = tilemap.GetTile(tilePosition);
+
+        // Create a new tile that uses the new sprite
+        Tile newTile = ScriptableObject.CreateInstance<Tile>();
+
+        newTile.sprite = matureSprite;
+        tilemap.SetTile(tilePosition, newTile);
+        Debug.Log("Plant " + tilePosition + " is mature!");
+
+        // Make the plant harvestable
+        harvestPlant(tilePosition, i);
+    }
+    
+    private void harvestPlant(Vector3Int tilePosition, int i){
+        // Get the current tile at the selected tile position
+        TileBase currentTile = tilemap.GetTile(tilePosition);
+
+        // Create a new tile that uses the new sprite
+        Tile newTile = ScriptableObject.CreateInstance<Tile>();
+        newTile.sprite = seedSprite;
+
+        // Change the sprite of the selected tile to the new sprite
+        tilemap.SetTile(selectedTilePosition, newTile);
+
+        StartCoroutine(WaitAndChangeTileToHarvestable(10f, tilePosition, i));
+    }
+
+    private IEnumerator WaitAndChangeTileToHarvestable(float waitTime, Vector3Int tilePosition, int i) {
+        yield return new WaitForSeconds(waitTime);
+
+        // Get the current tile at the selected tile position
+        TileBase currentTile = tilemap.GetTile(tilePosition);
+
+        // Create a new tile that uses the new sprite
+        Tile newTile = ScriptableObject.CreateInstance<Tile>();
+
+        // Change the sprite of the selected tile to the new sprite
+        newTile.sprite = harvestableSprites[i];
+
+        tilemap.SetTile(tilePosition, newTile);
+        Debug.Log("Plant " + tilePosition + " is harvestable!");
+
+        if(tilemap.GetTile(selectedTilePosition) == currentTile && Input.GetKeyDown(KeyCode.E) && isScytheInHand){
+            newTile.sprite = mudSprite;
+
+            tilemap.SetTile(tilePosition, newTile);
+            Debug.Log("Hurray!");
         }
     }
 }
