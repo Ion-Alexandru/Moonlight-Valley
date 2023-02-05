7597f6a897894e748ee1a33b1d7fc85d2b5df6c1
Ion-Alexandru <ion.alexandru2105@gmail.com>
2023-02-05 03:03:13 +0200

    Harvest and Combat Update

--- Assets/Script/gardeningBehaviour.cs
+++ Assets/Script/gardeningBehaviour.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Tilemaps;
+using static System.Net.Mime.MediaTypeNames;
 
 public class gardeningBehaviour : MonoBehaviour
 {
     public Tilemap tilemap;
+    public List<TileData> tileDataList;
     private Vector3Int selectedTilePosition;
     private Vector2 lastDirection = Vector2.zero;
 
@@ -19,26 +22,85 @@ public class gardeningBehaviour : MonoBehaviour
     public float speed;
     public GameObject seedOutline;
     public GameObject mudOutline;
-    int i = 0;
-    private bool isHoed;
-    private bool isHoeInHand = false;
-    private bool isSeedInHand = false;
-    private bool isScytheInHand = false;
-    float startTime;
+    public GameObject harvestOutline;
+    int seedIndex = 1;
+    int weaponIndex = 0;
+    public int[] vegetables = {0, 0};
+
+    public float timeToGrow;
+    public float timeToEvil;
+
+    private string[] seeds = {"None", "Carrot", "Tomato"};
+    private string[] weapons = {"Hand", "Hoe", "Seeds Pack", "Scythe"};
+    public UnityEngine.UI.Text currentSeed;
+    public UnityEngine.UI.Text currentWeapon;
+    public GameObject spritePrefab;
+    public Transform player;
+    private GameObject spawnedSprite;
+    private bool enemySpawned = false;
 
     private void Start() {
+        tileDataList = new List<TileData>();
+
+        currentSeed.text = "Current Seed: " + seeds[seedIndex];
+        currentWeapon.text = "Current Weapon: " + weapons[weaponIndex];
+
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if (tile != null)
+                {
+                    Vector3Int tilePosition = new Vector3Int(x + bounds.x, y + bounds.y, 0);
+                    tileDataList.Add(new TileData(tilePosition, 0, 0));
+                }
+            }
+        }
+
+        Debug.Log(tileDataList.Count);
     }
 
     private void Update() {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        if(Input.GetKeyDown(KeyCode.UpArrow) && i < 5){
-            i += 1;
-            Debug.Log("Current seed is:" + i);
-        } else if (Input.GetKeyDown(KeyCode.DownArrow) && i > 0){
-            i -= 1;
-            Debug.Log("Current seed is:" + i);
+        // spawn enemy
+        if (enemySpawned){
+            Vector3 targetPos = player.position;
+            float step = speed * Time.deltaTime;
+            spawnedSprite.transform.position = Vector3.MoveTowards(spawnedSprite.transform.position, targetPos, step);
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && seedIndex < seeds.Length - 1){
+            seedIndex += 1;
+
+            Debug.Log("Current seed: " + seeds[seedIndex]);
+            
+            currentSeed.text = "Current Seed: " + seeds[seedIndex];
+
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && seedIndex > 1){
+            seedIndex -= 1;
+
+            Debug.Log("Current seed: " + seeds[seedIndex]);
+            currentSeed.text = "Current Seed: " + seeds[seedIndex];
+        }
+        
+        float Scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (Scroll > 0 && weaponIndex < weapons.Length - 1) // forward
+        {
+            weaponIndex += 1;
+
+            currentWeapon.text = "Current Weapon: " + weapons[weaponIndex];
+        } else if (Scroll < 0 && weaponIndex > 0) // backwards
+        {
+            weaponIndex -= 1;
+
+            currentWeapon.text = "Current Weapon: " + weapons[weaponIndex];
         }
 
         Vector2 direction = new Vector2(horizontal, vertical);
@@ -49,43 +111,34 @@ public class gardeningBehaviour : MonoBehaviour
 
         transform.position = transform.position + new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
 
-        if (isHoeInHand){
+        if (weaponIndex == 0){
+            harvestOutline.SetActive(false);
+            seedOutline.SetActive(false);
+            mudOutline.SetActive(false);
+        }else if (weaponIndex == 1){
             mudOutline.transform.position = tilemap.GetCellCenterWorld(selectedTilePosition);
 
-            changeTileSprite();
-        }
+            harvestOutline.SetActive(false);
+            seedOutline.SetActive(false);
+            mudOutline.SetActive(true);
 
-        if (isSeedInHand){
+            makeTerrainMud();
+        }else if (weaponIndex == 2){
             seedOutline.transform.position = tilemap.GetCellCenterWorld(selectedTilePosition);
 
-            plantSeed(selectedTilePosition, i);
-        }
-
-        if(Input.GetKeyDown(KeyCode.Alpha1) && isHoeInHand == false){
-            isHoeInHand = true;
-            mudOutline.SetActive(true);
-
-        } else if(Input.GetKeyDown(KeyCode.Alpha1) && isHoeInHand) {
-            isHoeInHand = false;
+            harvestOutline.SetActive(false);
             mudOutline.SetActive(false);
-        }
-
-        if(Input.GetKeyDown(KeyCode.Alpha2) && isSeedInHand == false){
-            isSeedInHand = true;
             seedOutline.SetActive(true);
 
-        } else if(Input.GetKeyDown(KeyCode.Alpha2) && isSeedInHand) {
-            isSeedInHand = false;
+            plantSeed(selectedTilePosition, seedIndex);
+        } else if (weaponIndex == 3) {
+            harvestOutline.transform.position = tilemap.GetCellCenterWorld(selectedTilePosition);
+            
             seedOutline.SetActive(false);
-        }
-
-        if(Input.GetKeyDown(KeyCode.Alpha3) && isScytheInHand == false){
-            isScytheInHand = true;
-            mudOutline.SetActive(true);
-
-        } else if(Input.GetKeyDown(KeyCode.Alpha3) && isScytheInHand) {
-            isScytheInHand = false;
             mudOutline.SetActive(false);
+            harvestOutline.SetActive(true);
+
+            harvestPlant(selectedTilePosition);
         }
     }
 
@@ -94,7 +147,7 @@ public class gardeningBehaviour : MonoBehaviour
         selectedTilePosition = cellPosition + Vector3Int.RoundToInt(lastDirection);
     }
 
-    private void changeTileSprite() {
+    private void makeTerrainMud() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             // Get the current tile at the selected tile position
             TileBase currentTile = tilemap.GetTile(selectedTilePosition);
@@ -104,36 +157,55 @@ public class gardeningBehaviour : MonoBehaviour
             newTile.sprite = mudSprite;
 
             // Change the sprite of the selected tile to the new sprite
-            tilemap.SetTile(selectedTilePosition, newTile);
+            for (int j = 0; j < tileDataList.Count; j++)
+            {
+                if (tileDataList[j].position == selectedTilePosition && tileDataList[j].state == 0)
+                {
+                    tileDataList[j].state = 1;
+                    tileDataList[j].seed = 0;
+                    tilemap.SetTile(selectedTilePosition, newTile);
+                    break;
+                }
+            }
 
-            Debug.Log("Tile position is: " + selectedTilePosition);
-        } else if (Input.GetKeyDown(KeyCode.Backspace)){
-            // Get the current tile at the selected tile position
-            TileBase currentTile = tilemap.GetTile(selectedTilePosition);
+        } 
+        // else if (Input.GetKeyDown(KeyCode.Backspace)){
+        //     // Get the current tile at the selected tile position
+        //     TileBase currentTile = tilemap.GetTile(selectedTilePosition);
 
-            // Create a new tile that uses the new sprite
-            Tile newTile = ScriptableObject.CreateInstance<Tile>();
-            newTile.sprite = grassSprite;
+        //     // Create a new tile that uses the new sprite
+        //     Tile newTile = ScriptableObject.CreateInstance<Tile>();
+        //     newTile.sprite = grassSprite;
 
-            // Change the sprite of the selected tile to the new sprite
-            tilemap.SetTile(selectedTilePosition, newTile);
-        }
+        //     // Change the sprite of the selected tile to the new sprite
+        //     tilemap.SetTile(selectedTilePosition, newTile);
+        // }
     }
 
     private void plantSeed(Vector3Int tilePosition, int i) {
-        if (Input.GetKeyDown(KeyCode.F)) {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Vector3Int storedTilePosition = tilePosition;
+
             // Get the current tile at the selected tile position
-            TileBase currentTile = tilemap.GetTile(tilePosition);
+            TileBase currentTile = tilemap.GetTile(storedTilePosition);
 
             // Create a new tile that uses the new sprite
             Tile newTile = ScriptableObject.CreateInstance<Tile>();
             newTile.sprite = seedSprite;
 
             // Change the sprite of the selected tile to the new sprite
-            tilemap.SetTile(tilePosition, newTile);
+            for (int j = 0; j < tileDataList.Count; j++)
+            {
+                if (tileDataList[j].position == storedTilePosition && tileDataList[j].state == 1)
+                {
+                    tileDataList[j].state = 1;
+                    tileDataList[j].seed = i;
+                    tilemap.SetTile(storedTilePosition, newTile);
+                    break;
+                }
+            }
 
-            StartCoroutine(WaitAndChangeTileToMature(10f, tilePosition, i));
-
+            StartCoroutine(WaitAndChangeTileToMature(timeToGrow, storedTilePosition, i));
         }
     }
 
@@ -148,24 +220,13 @@ public class gardeningBehaviour : MonoBehaviour
 
         newTile.sprite = matureSprite;
         tilemap.SetTile(tilePosition, newTile);
-        Debug.Log("Plant " + tilePosition + " is mature!");
 
         // Make the plant harvestable
-        harvestPlant(tilePosition, i);
+        growToHarvestable(tilePosition, i);
     }
     
-    private void harvestPlant(Vector3Int tilePosition, int i){
-        // Get the current tile at the selected tile position
-        TileBase currentTile = tilemap.GetTile(tilePosition);
-
-        // Create a new tile that uses the new sprite
-        Tile newTile = ScriptableObject.CreateInstance<Tile>();
-        newTile.sprite = seedSprite;
-
-        // Change the sprite of the selected tile to the new sprite
-        tilemap.SetTile(selectedTilePosition, newTile);
-
-        StartCoroutine(WaitAndChangeTileToHarvestable(10f, tilePosition, i));
+    private void growToHarvestable(Vector3Int tilePosition, int i){
+        StartCoroutine(WaitAndChangeTileToHarvestable(timeToGrow, tilePosition, i));
     }
 
     private IEnumerator WaitAndChangeTileToHarvestable(float waitTime, Vector3Int tilePosition, int i) {
@@ -178,16 +239,90 @@ public class gardeningBehaviour : MonoBehaviour
         Tile newTile = ScriptableObject.CreateInstance<Tile>();
 
         // Change the sprite of the selected tile to the new sprite
-        newTile.sprite = harvestableSprites[i];
+        newTile.sprite = harvestableSprites[i-1];
 
-        tilemap.SetTile(tilePosition, newTile);
-        Debug.Log("Plant " + tilePosition + " is harvestable!");
+        for (int j = 0; j < tileDataList.Count; j++)
+        {
+            if (tileDataList[j].position == tilePosition)
+            {
+                Debug.Log("Plant " + tilePosition + " is harvestable!");
+                tileDataList[j].state = 2;
+                tilemap.SetTile(tilePosition, newTile);
+                break;
+            }
+        }
 
-        if(tilemap.GetTile(selectedTilePosition) == currentTile && Input.GetKeyDown(KeyCode.E) && isScytheInHand){
+        StartCoroutine(WaitAndChangeCropToEvil(timeToEvil, tilePosition));
+    }
+
+    private IEnumerator WaitAndChangeCropToEvil(float waitTime, Vector3Int tilePosition) {
+        yield return new WaitForSeconds(waitTime);
+
+        // Get the current tile at the selected tile position
+        TileBase currentTile = tilemap.GetTile(tilePosition);
+
+        // Create a new tile that uses the new sprite
+        Tile newTile = ScriptableObject.CreateInstance<Tile>();
+
+        // Change the sprite of the selected tile to the new sprite
+        newTile.sprite = mudSprite;
+
+        for (int j = 0; j < tileDataList.Count; j++)
+        {
+            if (tileDataList[j].position == tilePosition && tileDataList[j].state == 2)
+            {
+                Debug.Log("Plant " + tilePosition + " is now EVIL!"); 
+                tileDataList[j].state = 0;
+                tileDataList[j].seed = 0;
+                tilemap.SetTile(tilePosition, newTile);
+
+                spawnedSprite = Instantiate(spritePrefab, tilePosition, Quaternion.identity);
+                enemySpawned = true;
+                break;
+            }
+        }
+    }
+
+    private void harvestPlant(Vector3Int tilePosition){
+
+        if (Input.GetKeyDown(KeyCode.Space) && weaponIndex == 3)
+        {
+            // Get the current tile at the selected tile position
+            TileBase currentTile = tilemap.GetTile(tilePosition);
+
+            // Create a new tile that uses the new sprite
+            Tile newTile = ScriptableObject.CreateInstance<Tile>();
+
             newTile.sprite = mudSprite;
 
-            tilemap.SetTile(tilePosition, newTile);
-            Debug.Log("Hurray!");
+            for (int j = 0; j < tileDataList.Count; j++)
+            {
+                if (tileDataList[j].position == tilePosition && tileDataList[j].state == 2 && tileDataList[j].seed == 1 || tileDataList[j].seed == 2)
+                {   
+                    vegetables[tileDataList[j].seed - 1] += Random.Range(1, 3);
+                    Debug.Log("You now have " + seeds[tileDataList[j].seed] + ": " + vegetables[tileDataList[j].seed - 1]);
+
+                    tileDataList[j].state = 0;
+                    tileDataList[j].seed = 0;
+                    tilemap.SetTile(tilePosition, newTile);
+                    break;
+                }
+            }
+        }
+    }
+
+    public class TileData
+    {
+        public Vector3Int position;
+        public int state;
+        public int seed;
+
+        public TileData(Vector3Int position, int state, int seed)
+        {
+            this.position = position;
+            this.state = state;
+            this.seed = seed;
         }
     }
 }
+
